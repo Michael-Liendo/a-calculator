31ddc9f559b727f1299c5e65813cd3c478b5b6e1
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-02-13 13:48:34 -0400

    chore(add numbers and simbols to the buttons)

---
+++ app.js
@@ -0,0 +1,23 @@
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".eight");
+let plus = document.querySelector(".plus");
+
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let less = document.querySelector(".less");
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let x = document.querySelector(".x");
+
+let zero = document.querySelector(".zero");
+let point = document.querySelector(".point");
+let AC = document.querySelector(".AC");
+let division = document.querySelector(".division");
+
+function handlerFunction(number) {
+  console.log(number);
+}
